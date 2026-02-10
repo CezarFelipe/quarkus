63de6527b1006c552ef9da505cff1a6f5924dd09
Cezar Felipe <cezarfelipe2008@outlook.com>
2026-02-10 19:46:26 -0300

    feat: incluido exemplos de endpoint rest [CRUD]

---
+++ tutorial/src/main/java/org/brazona/domain/MovieDTO.java
@@ -0,0 +1,23 @@
+package org.brazona.domain;
+
+public class MovieDTO {
+
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
