7691f06f362c6e72dcbf22be509edf0732590377
Cezar Felipe <cezarfelipe2008@outlook.com>
2026-02-11 17:06:08 -0300

    feat: incluido exemplo de uso da lib RestClient

--- tutorial/src/main/java/org/brazona/app/resource/SeriesResource.java
+++ tutorial/src/main/java/org/brazona/app/resource/SeriesResource.java
@@ -1,42 +1,50 @@
 package org.brazona.app.resource;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.brazona.domain.dto.EpisodeDTO;
 import org.brazona.domain.dto.SeriesTvDTO;
-import org.brazona.domain.service.EpisodeTvService;
-import org.brazona.domain.service.SeriesTvService;
+import org.brazona.infra.proxy.EpisodeTvProxy;
+import org.brazona.infra.proxy.SeriesTvProxy;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.List;
+
 
 @Path("/series")
 public class SeriesResource {
 
-    @Inject
-    private SeriesTvService seriesTvService;
-    private EpisodeTvService episodeTvService;
-
+    @RestClient
+    SeriesTvProxy seriesTvProxy;
+    @RestClient
+    EpisodeTvProxy episodeTvProxy;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/search")
     public Response getSeries(@QueryParam("title") String title){
-        SeriesTvDTO seriesTvDTO = seriesTvService.get(title);
-        if (seriesTvDTO == null){
-            return Response.status(Response.Status.NOT_FOUND).build();
+        try {
+            System.out.println(title);
+            SeriesTvDTO seriesTvDTO = seriesTvProxy.getSeriesTV(title);
+            if (seriesTvDTO == null){
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            return Response.ok(seriesTvDTO).build();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
         }
-        return Response.ok(seriesTvDTO).build();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{id}/episode")
+    @Path("/{id}/episodes")
     public Response getEpisode(@PathParam("id") Long id){
-        EpisodeDTO episodeDTO = episodeTvService.getEpisode(id);
-        if (episodeDTO == null){
+        List<EpisodeDTO> episodesDTO = episodeTvProxy.getEpisodeTv(id);
+        if (episodesDTO.isEmpty()){
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        return Response.ok(episodeDTO).build();
+        return Response.ok(episodesDTO).build();
     }
 }
--- tutorial/src/main/java/org/brazona/infra/proxy/SeriesTvProxy.java
+++ tutorial/src/main/java/org/brazona/infra/proxy/SeriesTvProxy.java
@@ -3,17 +3,22 @@ package org.brazona.infra.proxy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import org.brazona.domain.dto.SeriesTvDTO;
-@ApplicationScoped
-@Path("/search")
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+//https://api.tvmaze.com/singlesearch/shows?q=girls
+
+@Path("/singlesearch")
 @Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient(baseUri = "http://api.tvmaze.com")
+@ApplicationScoped
 public interface SeriesTvProxy {
 
     @GET
     @Path("/shows")
-    SeriesTvDTO getSeriesTV(@PathParam(value = "q") String title);
+    SeriesTvDTO getSeriesTV(@QueryParam(value = "q") String title);
 
 }
